d8ddf722fb4984143d4c9c2fd2a116d7f5e26b14
vincent@pc <vincent@jfeat.com>
2019-02-22 12:13:02 +0800

    git status created_time

--- src/main/resources/sql/env-schema.sql
+++ src/main/resources/sql/env-schema.sql
@@ -4,7 +4,7 @@ CREATE TABLE `t_test_saas_entity`(
   `name` varchar(30) NOT NULL COMMENT '测试名称',
   `org_id` bigint(20) DEFAULT 0 COMMENT '所属组织',
   `status` varchar(20) NOT NULL COMMENT '状态',
-  `created_time` datetime NOT NULL COMMENT '创建时间',
+  `created_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   PRIMARY KEY (`id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
