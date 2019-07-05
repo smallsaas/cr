3a0ca2a7e3a04816e8bd50a06740a04be0bfc924
onSunday <996597885@qq.com>
2019-07-05 17:00:07 +0800

    feature: 支持swagger

--- src/main/java/com/jfeat/AmApplication.java
+++ src/main/java/com/jfeat/AmApplication.java
@@ -1,10 +1,15 @@
 package com.jfeat;
 
+import com.jfeat.am.config.properties.AmProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import javax.annotation.Resource;
 
 /**
  * SpringBoot方式启动类
@@ -14,9 +19,23 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  */
 @SpringBootApplication
 @EnableScheduling
-public class AmApplication{
+public class AmApplication extends WebMvcConfigurerAdapter {
+
+    @Resource
+    private AmProperties amProperties;
 
     protected final static Logger logger = LoggerFactory.getLogger(AmApplication.class);
+
+    /**
+     * 增加swagger的支持
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        if(amProperties.getSwaggerOpen()){
+            registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+            registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+        }
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AmApplication.class, args);
