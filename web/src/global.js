5b09c439b7efda0f4ae52f6349387f405551ff1a
hero <766653466@qq.com>
2019-12-19 18:24:05 +0800

    修复打包错误问题

--- web/src/global.js
+++ web/src/global.js
@@ -2,7 +2,7 @@ if(!window.MC){
   window.MC = {};
 }
 
-window.MC.HOST = 'http://106.12.212.64:8081';   //CR后台
+window.MC.HOST = 'http://115.231.156.18:8081';   //CR后台
 window.MC.HASH = '#';
-window.MC.BASEHOST = 'http://106.12.212.64:8088';  // 注册登录
-window.MC.SENDCODE = 'http://120.79.77.207:8080';  //验证码
+window.MC.BASEHOST = 'http://115.231.156.18:8088';  // 注册登录
+window.MC.SENDCODE = 'http://115.231.156.18:8088';  //验证码
